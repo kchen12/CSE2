0cc54d65f653d68255c0684a4430f94822891dde
kchen12 <kec317@lehigh.edu>
2014-08-31 18:19:14 +0000

    uploaded hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,14 @@
+public class WelcomeClass {
+    public static void main(String[] args) {
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" // \\// \\// \\// \\// \\// ");
+        System.out.println(" |-K--E--C--3--1--7-|");
+        System.out.println(" \\ //\\ //\\ //\\ //\\ //\\ ");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Hi, I'm Kevin Chen. I am from Taiwan!");
+        System.out.println("test");
+    }
+}
