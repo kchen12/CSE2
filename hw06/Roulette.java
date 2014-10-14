227457f87c64d1760793a9f547157740e3c05786
kchen12 <kec317@lehigh.edu>
2014-10-14 18:11:39 +0000

    uploaded Roulette

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -5,6 +5,7 @@
 //
 public class Roulette{
     public static void main(String[] args) {
+        
         int uNum = (int)(Math.random()*37);  // find a random number between 0 and 37
         // declare the variables
         int counter = 0;
@@ -14,7 +15,9 @@ public class Roulette{
         int totalWin = 0;
         int profitCount = 0;
         double aveWin = 0;
+        
         while(simCounter <1000){  // run the simulation 1000 times
+        
             while(counter < 100){  // simulate the 1$ bet 100 times
                 int rouNum = (int)(Math.random()*37);
                 if(rouNum == uNum){
@@ -22,19 +25,24 @@ public class Roulette{
                 }
                 counter++;
             }
+            
             if(matches == 0){  // find the number of times that you won nothing
                 noMatch = noMatch +1;
             }
+            
             if(matches >= 3){  // find the number of times you profited
                 profitCount = profitCount +1;
             }
+            
             totalWin = totalWin + matches;
             counter = 0;  // reset the counter
             matches = 0;  // reset the number of matches
             simCounter++;
         }
+        
         int total = totalWin*36;  // calculate the total profit
         aveWin = (totalWin/1000);
+        
         System.out.println("Your number is: " +uNum);
         System.out.println("Your total profit is: " +total);
         System.out.println("The number of times you won nothing is: " +noMatch);
